f18031e36c2c3ecbcda930d03c907b4d1155bb98
Sam9018 <Gnuschke@live.com>
2019-07-23 10:22:54 -0700

    setup

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # lab-11-ejs-templating
+
+Number and name of feature: Basic Set-up
+
+Estimate of time needed to complete: 50 minutes
+
+Start time: 9:00
+
+Finish time: _____
+
+Actual time needed to complete: _____
---
+++ server.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const express = require('express');
+const superagent = require('superagent');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static('./public'));
+app.set('view engine', 'ejs');
+
+// testing without data comment this next line out
+app.get('/hello',sayHello);
+function sayHello(request, response) {
+  response.reder('pages/index')
+};
+
+
+app.listen(PORT, () => console.log(`Listening on : ${PORT}`));
+
+app.get('/', newSearch);
+
+function newSearch(request, response) {
+  response.render('pages/index')
+};
