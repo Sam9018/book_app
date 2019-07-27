f283ca64ff1fb269d7a67d4154e256c8cca39215
leyla li <leylali@qq.com>
2019-07-26 17:10:24 -0700

    make update button works

--- public/js/app.js
+++ public/js/app.js
@@ -9,3 +9,14 @@ $('.dropForm').on('click',function(event){
 $('.show').on('click',function(){
 
 })
+
+$('.viewDetail').on('click',function(event){
+    const $id = $(event.target);
+    const value = $id.val();
+    console.log(value);
+    $.get(`/books/${value}`, ()=>{window.location.href = `/books/${value}`});
+});
+
+$('.updateDetail').on('click',function(event){
+    $().toggleClass('hide');
+})
